8f40a01589da8ff7c78596ff5ec3d0e70a1fafbd
Kimberly Grey <kimandthemachine@gmail.com>
2019-07-09 23:28:13 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="app">
+        <Clock />
+    </div>
+</template>
+
+<script>
+import Clock from './components/Clock.vue';
+export default {
+    name: 'app',
+    components: {
+        Clock
+    }
+}
+</script>
+
+<style>
+@import url('https://rsms.me/inter/inter.css');
+:root {
+	--highlight: #e5218c;
+}
+* {
+	box-sizing: border-box;
+}
+*::before,
+*::after {
+	box-sizing: inherit;
+}
+html,
+body {
+	margin: 0;
+}
+body {
+	color: #fff;
+	background-color: #000;
+	font-family: 'Inter', sans-serif;
+}
+@supports (font-variation-settings: normal) {
+	html { font-family: 'Inter var', sans-serif; }
+}
+</style>
