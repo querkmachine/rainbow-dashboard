bc72a844583bdee7b0ef68ed6b27887e12936d91
Kimberly Grey <hi+github@berly.kim>
2019-07-22 07:03:51 +0100

    Set up API

---
+++ src/components/Todoist.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>Todoist</div>
+</template>
+
+<script>
+import Axios from 'axios';
+export default {
+	name: 'Todoist',
+	data() {
+		return {
+
+		}
+	},
+	mounted() {
+		this.getTasks();
+	},
+	methods: {
+		getTasks: function() {
+			Axios
+				.get('https://api.todoist.com/rest/v1/tasks', {
+					headers: {
+						'Authorization': `Bearer ${process.env.VUE_APP_TODOIST_API_TOKEN}`
+					}
+				})
+				.then((response) => {
+					console.log(response.data);
+				})
+		}
+	}
+}
+</script>
+
+<style scoped>
+</style>
