251d6bea2ae15e8bbbcda67d9f92c64f64829c03
Kimberly Grey <hi+github@berly.kim>
2019-07-24 23:25:17 +0100

    Start filtering and outputting tasks

--- src/components/Todoist.vue
+++ src/components/Todoist.vue
@@ -1,18 +1,37 @@
 <template>
-	<div>Todoist</div>
+	<div>
+		<div>Todo today</div>
+		<ul>
+			<li v-for="item in tasksWithDueDates" :key="item.id">
+				{{ item.content }} — 
+				{{ item.due.date | dateDifference }}
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
 import Axios from 'axios';
+import Moment from 'moment';
 export default {
 	name: 'Todoist',
 	data() {
 		return {
-
+			tasks: []
 		}
 	},
 	mounted() {
 		this.getTasks();
+	},
+	computed: {
+		tasksWithDueDates: function() {
+			const tasks = this.tasks.filter(task => typeof task.due !== 'undefined');
+			return tasks.sort((a, b) => {
+				a = new Date(a.due.date);
+				b = new Date(b.due.date);
+				return a > b ? 1 : a < b ? -1 : 0;
+			})
+		}
 	},
 	methods: {
 		getTasks: function() {
@@ -23,8 +42,36 @@ export default {
 					}
 				})
 				.then((response) => {
-					console.log(response.data);
+					this.tasks = response.data;
 				})
+				.catch((error) => {
+					console.log(error);
+				})
+		}
+	},
+	filters: {
+		dateDifference: function(datetime) {
+			const today = Moment();
+			const taskDate = Moment(datetime);
+			console.log(taskDate, today.add(1, 'days'));
+			if(taskDate.isSame(today, 'day')) {
+				return 'today';
+			}
+			else if(taskDate.isSame(today.subtract(1, 'days'), 'day')) {
+				return 'yesterday';
+			}
+			else if(taskDate.isSame(today.add(1, 'days'), 'day')) {
+				return 'tomorrow';
+			}
+			else if(taskDate.isBefore(today)) {
+				return 'x days ago';
+			}
+			else if(taskDate.isAfter(today)) {
+				return 'in x days';
+			}
+			else {
+				return datetime;
+			}
 		}
 	}
 }
